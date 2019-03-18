45fa8b07cb1dea986da1cc033399c9ad5e1e5d2f
Saul <sc.gomez11@uniandes.edu.co>
2019-03-17 23:35:48 -0500

    Final update

--- 07-BSDS/save_cells.py
+++ 07-BSDS/save_cells.py
@@ -16,7 +16,7 @@ def save_cells(img_folder, K_vector):
     img_path= os.path.join('BSR','BSDS500','data','images',img_folder,'*.jpg')
     images=glob.glob(img_path)
     folder_w= img_folder + '/watershed_cells'
-    folder_k= img_folder +'/kmeans_cells'
+    folder_k= img_folder +'/kmeans_cells_filter'
     if not os.path.exists(img_folder):
         os.mkdir(img_folder)
     if not os.path.exists(folder_k):
@@ -26,7 +26,6 @@ def save_cells(img_folder, K_vector):
         
     for i in images:
         img = imageio.imread(i)
-        #img= GHF(i)
         
         Seg_watershed= np.zeros((len(K_vector)), dtype = np.object)
         Seg_kmeans= np.zeros((len(K_vector)), dtype = np.object)
@@ -34,9 +33,15 @@ def save_cells(img_folder, K_vector):
         for k in K_vector:   
             segmentation= segmentByClustering(img,'watershed',k)
             Seg_watershed[j]=segmentation
+            j+=1
+
+        img= GHF(i)
+        j=0
+        for k in K_vector:    
             segmentation= segmentByClustering(img,'kmeans',k)
             Seg_kmeans[j]=segmentation
-            j+=1
+            j+=1   
+
         name= i.split('/')
         name= name[5].split('.')
         name_watershed= folder_w + '/' + name[0] + '.mat'
